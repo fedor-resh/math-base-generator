b46a0111df29afb5998b0fb263b7fd6a47513b66
feduk <fedor_20052005@mail.ru>
2022-10-28 21:33:59 +0300

    update latex to python compiler

--- latex_to_python.py
+++ latex_to_python.py
@@ -1,15 +1,22 @@
 import re
+
+
 def latex_to_python(latex):
     latex = re.sub(r'\\frac\{([^}]+)\}\{([^}]+)\}', r'\1/\2', latex)
     latex = re.sub(r'\^\{([^}]+)\}', r'**\1', latex)
     latex = re.sub(r'\\sqrt\{([^}]+)\}', r'\1**0.5', latex)
-    latex = latex\
-        .replace('\\cdot', '*')\
-        .replace('\\left', '')\
-        .replace('\\right', '')\
-        .replace('[', '')\
+    latex = latex \
+        .replace('\\cdot', '*') \
+        .replace('\\left', '') \
+        .replace('\\right', '') \
+        .replace('[', '') \
         .replace(']', '')
     return latex
+
+
+def solve_latex_expression(latex):
+    return eval(latex_to_python(latex))
+
 
 if __name__ == '__main__':
     expression = r'\frac{2}{3} * \sqrt{2} * \left(\frac{2}{3}\right)^{2}'
