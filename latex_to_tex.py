ad59e2c59a102eb146828c63ed59ea271a891278
feduk <fedor_20052005@mail.ru>
2022-10-29 14:45:21 +0300

    refactor latex_to_tex.py

--- latex_to_tex.py
+++ latex_to_tex.py
@@ -4,20 +4,19 @@ import re
 def latex_to_tex(text):
     parts = text.split('$')
     for i in range(1, len(parts), 2):
-        parts[i] = parts[i].replace('*', '\cdot')
-        parts[i] = parts[i].replace(r'\frac', r'\\dfrac')\
-            .replace('\sqrt', r'\\sqrt')\
-            .replace('\left', r'\\left')\
-            .replace(r'\right', r'\\right')\
-            .replace('\cdot', r'\\cdot')\
-            .replace('{', '\{')\
-            .replace('}', '\}')\
+        parts[i] = parts[i].replace('*', r'\cdot')
+        parts[i] = parts[i].replace(r'\frac', r'\\dfrac') \
+            .replace(r'\sqrt', r'\\sqrt') \
+            .replace(r'\left', r'\\left') \
+            .replace(r'\right', r'\\right') \
+            .replace(r'\cdot', r'\\cdot') \
+            .replace('{', r'\{') \
+            .replace('}', r'\}')
+        parts[i] = r'\\(' + parts[i] + r'\\)'
 
-    text = '$'.join(parts)
-    text = re.sub(r'\$([^$]*)\$', r'\\\(\1\\\)', text)
+    text = ''.join(parts)
     return text
 
 
 if __name__ == '__main__':
-    print(latex_to_tex('$\sqrt{2}sdf{asdf} * 123${}$xxx$'))
-
+    print(latex_to_tex('$\sqrt{49}*{22}*123${=answer}$123$'))
