2009633aae99593fe209bb90f435060ae0969e48
fedor-resh <87930362+fedor-resh@users.noreply.github.com>
2022-11-14 16:01:50 +0300

    merge

--- latex_to_python.py
+++ latex_to_python.py
@@ -4,7 +4,7 @@ import re
 def latex_to_python(latex):
     latex = re.sub(r'\\frac\{([^}]+)\}\{([^}]+)\}', r'\1/\2', latex)
     latex = re.sub(r'\^\{([^}]+)\}', r'**\1', latex)
-    latex = re.sub(r'\\sqrt\{([^}]+)\}', r'(\1)**0.5', latex)
+    latex = re.sub(r'\\sqrt\{([^}]+)\}', r'\1**0.5', latex)
     latex = latex \
         .replace('\\cdot', '*') \
         .replace('\\left', '') \
@@ -13,13 +13,16 @@ def latex_to_python(latex):
         .replace(']', '')
     return latex
 
+def python_to_latex(python):
+    python = re.sub(r'([^/])\*([^/])', r'\1\cdot \2', python)
+    python = re.sub(r'([^/])\*\*0\.5', r'\sqrt{\1}', python)
+    python = re.sub(r'([^/])/([^/])', r'\frac{\1}{\2}', python)
+    python = re.sub(r'([^/])\*\*([^/])', r'\1^{\2}', python)
+    return python
 
 def solve_latex_expression(latex):
     return eval(latex_to_python(latex))
 
 
 if __name__ == '__main__':
-    print(latex_to_python(r'-\sqrt{7^{4}\cdot1^{4}}'))
-
-    print(eval(latex_to_python(r'-\sqrt{7^{4}\cdot1^{4}}')))
-
+    print(python_to_latex('1/2*3**0.5'))
