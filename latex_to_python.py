1fdf2859b071e033c80211f3d56c313aeeffe870
feduk <fedor_20052005@mail.ru>
2022-11-23 19:19:16 +0300

    update python to latex

--- latex_to_python.py
+++ latex_to_python.py
@@ -18,7 +18,7 @@ def latex_to_python(latex):
 
 def python_to_latex(python):
     """:return: latex string"""
-    python = re.sub(r'\((.*)\)\*\*0\.5', r'\\sqrt{\1}', python)
+    python = re.sub(r'\((([^()]*(\([^()]*\))*[^()]*)*)\)\*\*0\.5', r'\\sqrt{\1}', python)
     python = re.sub(r'(.)\*\*0\.5', r'\\sqrt{\1}', python)
 
     python = re.sub(r'(\(.+\)|.)\*\*(\(.+\)|\\[^}]+}|.)', r'\1^{\2}', python)
@@ -26,26 +26,59 @@ def python_to_latex(python):
     while '/' in python:
         copy = python
         print(python)
-        python = re.sub(r'(\((.+)\)|([^({})+-]*)(\{[^({})+-]*})*[^({})+-]*)'
-                        r'/(\((.+)\)|([^({})+/-]*)({.*})*[^({})+/-]*)', r'\\frac{\1}{\5}', python, count=1)
+        # python = re.sub(r'(\(([^()]*)(\([^()]*\))*([^()]*)\)|([^({})+-]*)({.*})*[^({})+-]*)'
+        #                 r'/(\(([^()]*)(\([^()]*\))*([^()]*)\)|([^({})+/-]*)({.*})*[^({})+/-]*)', r'\\frac{\1}{\7}',
+        #                 python, count=1)
+        python = re.sub(r'\((([^()]*(\([^()]*\))*[^()]*)*)\)'
+                        r'/\((([^()]*(\([^()]*\))*[^()]*)*)\)',
+                        r'\\frac{\1}{\4}', python)
+        python = re.sub(r'\(([^()]*)'
+                        r'/([^()]*)\)',
+                        r'\\frac{\1}{\2}', python)
+
         if copy == python:
             print('ERROR')
             break
+    print('exit' + python)
     latex = python.replace('(', r'\left(').replace(')', r'\right)')
+    render_latex(latex)
     return latex
 
 
-# def python_to_latex(python):
-#     stack = []
-#     while python:
-#         part_regex = r'(\*\*)|([\(\)\+\-\*/.0-9])'
-#         part = re.search(part_regex, python).group()
-#         stack.append(part)
-#         python = python[len(part):]
-#     print(stack)
+def render_latex(tex):
+    import matplotlib.pyplot as plt
+    tex = f'${tex}$'
+    # Создание области отрисовки
+    fig = plt.figure()
+    ax = fig.add_axes([0, 0, 1, 1])
+    ax.set_axis_off()
+
+    # Отрисовка формулы
+    t = ax.text(0.5, 0.5, tex,
+                horizontalalignment='center',
+                verticalalignment='center',
+                fontsize=20, color='black')
+
+    try:
+        # Определение размеров формулы
+        ax.figure.canvas.draw()
+        bbox = t.get_window_extent()
+        # print(bbox.width, bbox.height)
+
+        # Установка размеров области отрисовки
+        fig.set_size_inches(bbox.width / 80, bbox.height / 80)  # dpi=80
+
+        # Отрисовка или сохранение формулы в файл
+        plt.show()
+        # plt.savefig('test.svg')
+        # plt.savefig('test.png', dpi=300)
+    except Exception as e:
+        print(e)
+
+
 def solve_latex_expression(latex):
     return eval(latex_to_python(latex))
 
 
 if __name__ == '__main__':
-    print(python_to_latex('1/(2/(1/2))**0.5**2'))
+    print(python_to_latex('((2/3)(12.96)**0.5)/((1/7)(4.41)**0.5)'))
