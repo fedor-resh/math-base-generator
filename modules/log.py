d98f66e67736de21e8523399cc0b7216cc7f200d
fedor-resh <fedor_20052005@mail.ru>
2023-01-19 21:40:49 +0300

    fix

--- modules/log.py
+++ modules/log.py
@@ -5,7 +5,10 @@ from latex import latex_to_python, python_to_latex, render_latex
 # from latex import latex_to_python
 
 task = r'''
-Найдите значение выражения $\frac{1}{[a]}*(\log_{2}{([c]-[d])}^{[e]})^{[f]}-\frac{\log_{10}{([h]-[i])}}{\log_{10}{[k]}}*[l]^{[m]*\log_{2}{\sqrt{[o]}}}$
+Найдите значение выражения $\frac{\log_{[a]}{\log_{[b]}{[c]^{[d]}}}}{\log^{[e]}_{[f]}{[g]^{[h]}}}$
+'''
+task = r'''
+Найдите значение выражения $\frac{1}{[a]}*(\log_{2}{([c])}^{[e]})^{[f]}-\frac{\log_{10}{([h])}}{\log_{10}{[k]}}*[l]^{[m]*\log_{2}{\sqrt{[o]}}}$
 '''
 
 ranges = dict(
