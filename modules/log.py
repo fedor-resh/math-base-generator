972b3b4c50681a92015daaae38520ee459a331bc
fedor-resh <fedor_20052005@mail.ru>
2023-01-20 07:01:42 +0300

    fix

--- modules/log.py
+++ modules/log.py
@@ -8,18 +8,18 @@ task = r'''
 Найдите значение выражения $\frac{\log_{[a]}{\log_{[b]}{[c]^{[d]}}}}{\log^{[e]}_{[f]}{[g]^{[h]}}}$
 '''
 task = r'''
-Найдите значение выражения $\frac{1}{[a]}*(\log_{2}{([c])}^{[e]})^{[f]}-\frac{\log_{10}{([h])}}{\log_{10}{[k]}}*[l]^{[m]*\log_{2}{\sqrt{[o]}}}$
+Найдите значение выражения $\frac{1}{[a]}*(\log^{[e]}_{2}{[c]})^{[f]}-\frac{\log_{10}{[h]}}{\log_{10}{[k]}}*[l]^{[m]*\log_{2}{\sqrt{[o]}}}$
 '''
 
 ranges = dict(
     a=range(2, 10),
     b=range(2, 10),
-    c=range(2, 10),
+    c=range(3, 10),
     d=range(2, 10),
     e=range(2, 10),
     f=range(2, 10),
     g=range(2, 10),
-    h=range(2, 10),
+    h=range(10, 110),
     i=range(2, 10),
     k=range(2, 10),
     l=range(2, 10),
